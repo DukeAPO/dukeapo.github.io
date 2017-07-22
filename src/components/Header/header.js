40b0f7b5bb8fb7016d26e507e79648aeb11247ff
Briguy52 <Briguy52@gmail.com>
2017-07-21 21:21:40 -0400

    added responsive (white) navbar

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Scroll from 'react-scroll'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import './navbar.css';
 import classes from './Header.scss'
 
 class Header extends React.Component {
+
+  scrollToElement (elem) {
+    Scroll.scroller.scrollTo(elem, {
+      duration: 850,
+      delay: 100,
+      smooth: true
+    })
+  }
 
   render () {
     return (
@@ -15,12 +26,16 @@ class Header extends React.Component {
           </Navbar.Header>
           <Navbar.Collapse>
             <Nav>
-              <NavItem className={classes.navItem} href={'#'}>HOME</NavItem>
-              <NavItem className={classes.navItem} href={'#'}>ABOUT</NavItem>
-              <NavItem className={classes.navItem} href={'#'}>COMMUNITY</NavItem>
-              <NavItem className={classes.navItem} href={'#'}>RECRUITMENT</NavItem>
-              <NavItem className={classes.navItem} href={'#'}>MEMBERS</NavItem>
-              <NavItem className={classes.navItem} href={'#'}>SERVICE</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('homeScrollPoint')}>Home</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('aboutScrollPoint')}>About</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('communityScrollPoint')}>Community</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('recruitmentScrollPoint')}>Recruitment</NavItem>
+              <NavItem className={classes.navItem} onClick={() =>
+                this.scrollToElement('serviceScrollPoint')}>Service</NavItem>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
